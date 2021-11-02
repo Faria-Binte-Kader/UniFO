190df88af788d528ad4e3bf7cab259c2201f6bbd
Faria-Binte-Kader <fariabintekader67@gmail.com>
2021-11-02 10:14:26 +0600

    Signup,login

---
+++ learn/src/Components/useLoginForm.js
@@ -0,0 +1,32 @@
+import { useState, useEffect} from "react";
+import validateInfoLogin from "./validateInfoLogin";
+import  Axios from "axios";
+
+const useLoginForm = validateInfoLogin =>{
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    })
+
+    const [errors, setErrors] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const handleChange = e =>{
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setErrors(validateInfoLogin(values));
+        setIsSubmitted(true); 
+       
+    }
+
+    return { handleChange, values, handleSubmit, errors};
+
+};
+
+export default useLoginForm;
--- server/index.js
+++ server/index.js
@@ -1,27 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql')
+const cors = require('cors')
 const app=express()
 const port = process.env.PORT || 3001
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cors());
 
 
-const pool = mysql.createPool({
-   connectionLimit : 10,
-   host            : 'localhost',
+const db = mysql.createConnection({
    user            : 'root',
+   host            : 'localhost',
    password        : "",
    database        : 'UniFO'
 })
 
-app.get("/", (req, res) => {
-    const query = "SELECT * FROM test";
-    pool.query(query, (error, results) => {
-        if (error)  return console.error(error.message);
-        res.send(results);
-    })
+app.post("/", (req, res) => {
+    const Name = req.body.username;
+    const Type = req.body.usertype;
+    const Password = req.body.password;
+    const Email = req.body.email;
+
+    db.query("INSERT INTO users (name,type,password,email) VALUES (?,?,?,?)",
+    [Name,Type,Password,Email], (error, results) => {
+        if (error)  return console.log(error.message);
+        //res.send(results);
+    });
 });
+
+app.post("/login", (req, res) => {
+    const Password = req.body.password;
+    const Email = req.body.email;
+
+    db.query("SELECT email, password FROM users WHERE email = (?) AND password = (?)",
+    [Email,Password,], (error, results) => {
+        if (error)  {res.send({err: err})}
+            if(results.length>0)
+            {res.send(results)}
+            else {res.send({message: "Wrong username/password combination!"})}
+        //res.send(results);
+    });
+});
+
 
 app.listen(port, () =>{
     console.log("running on port 3001");
