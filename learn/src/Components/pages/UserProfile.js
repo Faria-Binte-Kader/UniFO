d78e7be7c93e767c03f12584d067ef257789d441
nhnujhat <nhnujhat@gmail.com>
2021-11-03 00:37:47 +0600

    profile now working

--- learn/src/Components/pages/UserProfile.js
+++ learn/src/Components/pages/UserProfile.js
@@ -1,13 +1,57 @@
-import React from "react";
-import '../../App.css';
-import Cards_UserProfile from "../Cards_UserProfile";
+import "./UserProfile.css";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Axios from "axios";
 
 function UserProfile() {
-    return (
-        <>
-            <Cards_UserProfile/>
-        </>
-    )
+    let { uname } = useParams();
+    const [name, setName] = useState("");
+    const [mail, setMail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [father, setFather] = useState("");
+    const [mother, setMother] = useState("");
+    const [gender, setGender] = useState("");
+    const [dob, setDob] = useState("");
+    const [blood, setBlood] = useState("");
+    const [address, setAddress] = useState("");
+
+    useEffect(() => {
+        Axios.get('http://localhost:3001/users').then((response) => {
+        setName(response.data.name);
+        setMail(response.data.mail);
+        setPhone(response.data.phone);
+        setFather(response.data.father);
+        setMother(response.data.mother);
+        setGender(response.data.gender);
+        setDob(response.data.dob);
+        setBlood(response.data.blood);
+        setAddress(response.data.address);
+        });
+      }, []);
+
+      return (
+        <div className="users">
+            <div>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+            <h3>{name}</h3>
+            <h3>{mail}</h3>
+            <h3>{phone}</h3>
+            <h3>Father's Name: {father}</h3>
+            <h3>Mother's Name: {mother}</h3>
+            <h3>Gender: {gender}</h3>
+            <h3>Date of Birth: {dob}</h3>
+            <h3>Blood Type: {blood}</h3>
+            <h3>Address: {address}</h3>
+            </div>
+        </div>
+      );
 }
 
 export default UserProfile
--- server/index.js
+++ server/index.js
@@ -8,13 +8,40 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 
-
 const db = mysql.createConnection({
-   user            : 'root',
-   host            : 'localhost',
-   password        : "",
-   database        : 'UniFO'
-})
+    user            : 'root',
+    host            : 'localhost',
+    password        : "",
+    database        : 'UniFO'
+ })
+ 
+app.post("/users", (req, res) => {
+    /*const Uname = req.body.uname;
+    const Name = req.body.name;
+    const Mail = req.body.mail;
+    const Phone = req.body.phone;
+    const Father = req.body.father;
+    const Mother = req.body.mother;
+    const Gender = req.body.gender;
+    const Dob = req.body.mdob;
+    const Blood = req.body.blood;
+    const Address = req.body.address;*/
+    const Uname = req.body.username;
+    const Mail = req.body.email;
+    const Phone = "";
+    const Father = "";
+    const Mother = "";
+    const Gender = "";
+    const Dob = "";
+    const Blood = "";
+    const Address = "";
+
+    db.query("INSERT INTO users(Uname,Name,Mail,Phone,Father,Mother,Gender,Dob,Blood,Address) VALUES (?,?,?,?,?,?,?,?,?,?)",
+    [Uname,Name,Mail,Phone,Father,Mother,Gender,Dob,Blood,Address], (error, results) => {
+        if (error)  return console.log(error.message);
+        //res.send(results);
+    });
+});
 
 app.post("/", (req, res) => {
     const Name = req.body.username;
@@ -34,7 +61,7 @@ app.post("/login", (req, res) => {
     const Email = req.body.email;
 
     db.query("SELECT email, password FROM users WHERE email = (?) AND password = (?)",
-    [Email,Password,], (error, results) => {
+    [Email,Password], (error, results) => {
         if (error)  {res.send({err: err})}
             if(results.length>0)
             {res.send(results)}
@@ -43,6 +70,12 @@ app.post("/login", (req, res) => {
     });
 });
 
+app.get("/", (req, res) => {
+    db.query("SELECT * FROM users", (error, results) => {
+        if (error)  return console.error(error.message);
+        res.send(results);
+    })
+});
 
 app.listen(port, () =>{
     console.log("running on port 3001");
