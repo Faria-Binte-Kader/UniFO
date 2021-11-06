cc3e1fa8667c5b54c2c1a673d35b6012b40fcb1b
Faria-Binte-Kader <fariabintekader67@gmail.com>
2021-11-06 19:50:58 +0600

    notices show

---
+++ learn/src/Components/pages/NoticeDetails.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './NoticeDetails.css'
+import { Link, useHistory  } from 'react-router-dom';
+import CardItem from '../CardItem'
+import { useEffect, useState } from "react";
+import  Axios from "axios";
